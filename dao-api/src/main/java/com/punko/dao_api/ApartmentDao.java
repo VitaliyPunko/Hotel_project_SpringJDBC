9f3968fdd72babcc152a7f07d8b0049816e048da
Vitaliy Punko <punkovitaliy@gmail.com>
2021-04-27 20:46:34 +0300

    added doc for Dao-api

--- dao-api/src/main/java/com/punko/dao_api/ApartmentDao.java
+++ dao-api/src/main/java/com/punko/dao_api/ApartmentDao.java
@@ -4,19 +4,55 @@ import com.punko.Apartment;
 
 import java.util.List;
 
+/**
+ * ApartmentDao DAO Interface.
+ */
 public interface ApartmentDao {
 
+    /**
+     * Get all apartments with average time between
+     * arrival time and departure time.
+     * @return apartment list.
+     */
     List<Apartment> findAll();
 
+    /**
+     * Get apartment by id
+     * @param apartmentId apartment id
+     * @return apartment.
+     */
     Apartment findById(Integer apartmentId);
 
+    /**
+     * Create new apartment
+     * @param apartment new Apartment
+     * @return count of added apartments.
+     */
     Integer create(Apartment apartment);
 
+    /**
+     * Update apartment
+     * @param apartment Apartment
+     * @return count of updated apartments.
+     */
     Integer update(Apartment apartment);
 
+    /**
+     * Delete apartment
+     * @param apartmentId apartment id
+     * @return count of deleted apartments.
+     */
     Integer delete(Integer apartmentId);
 
+    /**
+     * Count of all apartments
+     * @return count of apartments.
+     */
     Integer count();
 
+    /**
+     * Get all apartment's classes
+     * @return apartment's classes list.
+     */
     List<String> getAllApartmentClass();
 }
--- dao-api/src/main/java/com/punko/dao_api/ResidentDao.java
+++ dao-api/src/main/java/com/punko/dao_api/ResidentDao.java
@@ -6,23 +6,66 @@ import com.punko.Resident;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * ResidentDao DAO Interface.
+ */
 public interface ResidentDao {
 
+    /**
+     * Get all residents
+     * @return resident list.
+     */
     List<Resident> findAll();
 
+    /**
+     * Create new resident
+     * @param resident new Resident.
+     */
     void create(Resident resident);
 
+    /**
+     * Get all exist apartment's number
+     * @return apartment list.
+     */
     List<Apartment> getAllApartmentNumber();
 
-    Resident findById(Integer id);
+    /**
+     * Get resident by id
+     * @param residentId resident id
+     * @return resident.
+     */
+    Resident findById(Integer residentId);
 
+    /**
+     * Update resident
+     * @param resident Resident
+     */
     void updateResident(Resident resident);
 
-    Integer delete(Integer id);
+    /**
+     * Delete resident
+     * @param residentId resident id
+     * @return count of deleted residents.
+     */
+    Integer delete(Integer residentId);
 
+    /**
+     * Get residents from arrivalTime to departureTime
+     * @param arrivalTime resident arrivalTime
+     * @param departureTime resident departureTime
+     * @return resident list.
+     */
     List<Resident> findAllByTime(LocalDate arrivalTime, LocalDate departureTime);
 
+    /**
+     * Count of all residents
+     * @return count of residents.
+     */
     Integer count();
 
+    /**
+     * Get residents order by min arrival time
+     * @return resident list order by min arrival time.
+     */
     List<Resident> orderByDate();
 }
